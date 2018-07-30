d9487faf2cd6dd26c3409e4e58fca9cd1e197c4c
杜广全 <duguangquan@duguangquandeMacBook-Pro.local>
2018-07-31 00:18:13 +0800

    多模块

---
+++ model/src/main/java/com/obelisk/model/User.java
@@ -0,0 +1,27 @@
+package com.obelisk.model;
+
+/**
+ * Created by duguangquan on 2018/7/29.
+ */
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
